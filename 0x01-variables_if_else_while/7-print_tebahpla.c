aec812c887291dbc7859a29c8ce2ee31725683d4
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-03-18 14:47:59 +0100

    mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+* main -Entry point
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+char c;
+for (c = 'z'; c >= 'a'; c--)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
