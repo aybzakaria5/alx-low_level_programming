7121ff7fe2a03a20e564c26dc2ed036ae5d463ec
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-03-18 13:28:07 +0100

    no e or q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,24 +1,23 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 /**
-* main -printing the alphabet in lowcase
-* except for e and q
-*
-* Return: Aalways 0 (Success)
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-char alph = 'a';
-while
-(alph <= 'z');
-{
-if
-(alph != 'q' && alph != 'e')
-{
-putchar(alph);
-}
-alph++;
-}
-putchar('\n');
-return (0);
+	char alph;
+
+	alph = 'a';
+	while (alph <= 'z')
+	{
+		if (alph != 'q' && alph != 'e')
+		{
+			putchar(alph);
+		}
+		alph++;
+	}
+	putchar('\n');
+	return (0);
 }
