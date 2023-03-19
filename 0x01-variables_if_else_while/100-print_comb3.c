8a399714db0864b230d1e829a07fdf406514fceb
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-03-19 19:33:36 +0100

    combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+/**
+* main - Entry point
+* Return: Always 0
+*/
+
+int main(void)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (i <= 8)
+	{
+		j = 1;
+		while (j <= 9)
+		{
+			putchar(i + 48);
+			putchar(j + 48);
+			if (i != 8 || j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
