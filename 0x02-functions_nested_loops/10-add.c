c28b56ff4457e8bab1978e958470ab0c12cd4fb4
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-03-22 02:36:45 +0100

    adding two intregers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - add 2 intregers
+ *@a: first input
+ *@b: second input
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
