b72b3d5361ac1299cb0cfcaf0703046969a76ebb
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-03-24 00:43:35 +0100

    square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,7 +12,7 @@ void print_square(int size)
 
 	for (row = 1; row <= size; row++)
 	{
-		for (col = 1, col <= size; col++)
+		for (col = 1; col <= size; col++)
 			_putchar('#');
 		_putchar('\n');
 	}
