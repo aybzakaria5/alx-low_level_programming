a8e664c97491f28d47c050b8b2e3c877addb1f41
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-03-28 01:33:26 +0200

    lies

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * puts2 -  prints every other character
+ *@str: char input
+ */
+
+void puts2(char *str)
+{
+	int a;
+
+	for (a = 0 ; str[a] != '\0' ; a++)
+	if (a % 2 == 0)
+		_putchar(str[a]);
+	_putchar('\n');
+}
