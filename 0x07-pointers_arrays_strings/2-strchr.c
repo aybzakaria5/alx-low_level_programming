849ff34e3ffbc12658bf7f8419e728af1d120426
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-04-03 18:19:12 +0200

    i dunno what is the problem

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,12 +9,13 @@
 
 char *_strchr(char *s, char c)
 {
+	unsigned int i;
 
-	while (*s != '\0')
-		{
-			if (*s == c)
-			return (s);
-			s++;
-		}
-	return(NULL);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s+i);
+	}	
+	
+	return ('\0');
 }
