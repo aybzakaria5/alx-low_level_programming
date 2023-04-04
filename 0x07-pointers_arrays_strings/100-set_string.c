5c7773cb341265a32d9278a7eb2663d84b68060e
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-04-05 00:01:41 +0200

    sets the value of a p to char

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - a function that sets a pointer to a char
+ *@s:represented a spointer to pointer
+ *@to:represented as a pointer to a char
+ *Return: 0
+ */
+
+void set_string(char **s, char *to)
+
+{
+	*s = to;
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,3 +1,5 @@
+
+void set_string(char **s, char *to);
 int _putchar(char c);
 void print_chessboard(char (*a)[8]);
 char *_memset(char *s, char b, unsigned int n);
