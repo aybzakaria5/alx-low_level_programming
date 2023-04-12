98921caf8cac403c0abea5797855163271907d96
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-04-13 00:52:13 +0200

    conact

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -29,11 +29,13 @@ char *str_concat(char *s1, char *s2)
 		size_s1 = _strlen(s1);
 		size_s2 = _strlen(s2);
 	if (s1 == NULL)
+	{
 		s1 = 0;
-
+	}
 	if (s2 == NULL)
+	{
 		s2 = 0;
-
+	}
 	concat = malloc((size_s1 + size_s2 + 1) * sizeof(char));
 	if (concat == 0)
 	return (NULL);
