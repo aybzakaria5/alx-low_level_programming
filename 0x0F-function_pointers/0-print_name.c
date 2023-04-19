ca68741844fac54a6d158471bd0ba4f9911588ad
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-04-19 23:10:21 +0200

    printing using function pointer

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,12 @@
 #include "function_pointers.h"
 
 /**
- *pirnt_name - a function that prints a name
+ * print_name - a function that prints a name
  *@name: the string name
  *@f: the function pointer we will use to print name
- *Return;  void
+ *Return:  void
  */
- void print_name(char *name, void(*f)(char *))
+void print_name(char *name, void(*f)(char *))
 {
 	if (f)
 	f(name);
