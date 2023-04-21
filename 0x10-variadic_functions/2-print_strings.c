7b09936da51f22cbaa54877f222cd925599bed8b
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-04-22 01:08:44 +0200

    string with separator

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,24 @@
+#include"variadic_functions.h"
+
+/**
+ *print_strings - a function that prints string with separator
+ *@n: number of arguments
+ *@separator: separator to be between chars
+ *@...: the strings
+ *Return: void
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+	char *s;
+
+	va_start(args, n);
+	for (i =0; i < n; i++)
+	{
+		printf("%s%s", (s = va_arg(args, char *)) ? s : "(nil)",
+				i ? (separator ? separator : "") : "\n");
+		va_end(args);
+	}
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,6 +2,7 @@
 #define VARIADRIC_FUNCTIONS_H
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 #include<stdio.h>
 #include<stdarg.h>
 
