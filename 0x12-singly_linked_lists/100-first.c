e688cb7d4333feb8b548cee134d0241fd63683d4
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-04-28 02:44:15 +0200

    befor main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include<estdio.h>
+
+void before_main(void) __attribute__((constructor));
+/**
+ *befor_main - a function that prents a sentence before main
+ */
+void befor_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n);
+}
+
