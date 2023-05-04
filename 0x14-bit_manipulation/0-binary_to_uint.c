4cfef3a699482bbc190c4358b1842ef96f578a2c
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-05-04 23:54:11 +0200

    from binary to ui

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *binary_to_uint - a function that converts a binary num to ui
+ *@b: a pointer to a string
+ *Return: the converted num if success or 0 if not
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i = 0,  val = 0;
+
+	for (; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+		{
+			return (0);
+		}
+		val = val * 2 + (*b - '0');
+	}
+	return (val);
+}
