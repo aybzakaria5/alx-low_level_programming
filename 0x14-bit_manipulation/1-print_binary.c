e559434f8f3e0067ac699033f7199b284356926f
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-05-05 19:55:20 +0200

    perenting binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *print_binary - a function that prints binary
+ *@n: the input number
+ *
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int Sbit = 1;
+
+	while (Sbit <= n)
+	Sbit <<= 1;
+
+	if (Sbit > n)
+		Sbit >>= 1;
+
+	if (!n)
+		_putchar('0');
+
+	while (Sbit)
+	{
+		_putchar((n & Sbit) ? '1' : '0');
+		Sbit >>= 1;
+	}
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,6 +3,8 @@
 #include <stdio.h>
 unsigned int binary_to_uint(const char *b);
 
+void print_binary(unsigned long int n);
+int _putchar(char c);
 
 
 
