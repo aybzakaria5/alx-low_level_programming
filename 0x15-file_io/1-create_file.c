514c7d43efb34acfaaee1ac45a12d5e0ee3812e8
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-05-09 01:46:39 +0200

    last chance

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *create_file - a function that creates a file
+ *@filename: the file name
+ *@text_content: the text
+ *Return: 1 if success -1 if not
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, i = 0;
+	ssize_t n_write;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+	while (text_content[i])
+	{
+		i++;
+		n_write =  write(fd, text_content, i);
+	}
+	close(fd);
+	return ((n_write == -1) ? -1 : 1);
+}
