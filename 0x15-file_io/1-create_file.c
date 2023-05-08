20d962419878821c87a5e2a6e8fd0f5008e50701
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-05-09 00:52:31 +0200

    fixing the closing of the file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,20 +14,11 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	else
-		return (1);
-
-
-	if (text_content == NULL)
-	{
-		fd = creat(filename, 0600);
-		return (1);
-	}
-	fd = creat(filename, 0600);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	write(fd, text_content, sizeof(text_content));
 
 	if (fd == -1)
 		return (-1);
-
+	close(fd);
 			return (1);
 }
