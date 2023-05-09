61e2e269f1208ec8818e909c7cd0f791e63dcf1d
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-05-09 02:05:45 +0200

    !

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,12 +14,10 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 
-	fd = open(filename, O_RDWR, O_CREAT, O_TRUNC, 0600);
-
-	if (!fd)
+	if (fd == -1)
 		return (-1);
-
 	while (text_content[i])
 	{
 		i++;
