effdbd871312df74430f70a34308c7cca43b1338
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-05-09 02:01:13 +0200

    these checkers

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,9 +14,9 @@ int create_file(const char *filename, char *text_content)
 
 	fd = open(filename, O_RDWR, O_CREAT, O_TRUNC, 0600);
 
-	if (!filename|| !fd)
+	if (!filename || !fd)
 		return (-1);
-	
+
 	while (text_content[i])
 	{
 		i++;
