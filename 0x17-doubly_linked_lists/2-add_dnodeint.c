cf0aa72832e163ca97de7e3ac3a3c6e97b62e5a5
Ayoub Zakarya <85744427+aybzakaria5@users.noreply.github.com>
2023-06-15 21:37:49 +0000

    returning the num of elements

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -14,7 +14,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
-    return (NULL); 
+        return (NULL);
 	new_node->n = n;
 
 	if (*head == NULL)
