e6eefa89957fa483ea3a9f09e37159a563a612f9
Ayoub Zakarya <85744427+aybzakaria5@users.noreply.github.com>
2023-06-15 20:57:38 +0000

    printintg a dlsit

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,7 +2,7 @@
 
 /**
  * print_dlistint - print all the elements in a linked list.
- * h - head of linked list
+ * @h:  head of linked list
  *
  * Return: Number of nodes
  */
@@ -10,7 +10,7 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t elem = 0;
-    
+
 	while (h)
 	{
 		elem++;
