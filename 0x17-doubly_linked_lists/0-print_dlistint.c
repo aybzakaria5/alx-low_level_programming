177574dcb9388614ad8ca788f43b37e8679ab31a
Ayoub Zakarya <85744427+aybzakaria5@users.noreply.github.com>
2023-06-15 20:28:04 +0000

    printing dlist

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - print all the elements in a linked list.
+ * h - head of linked list
+ *
+ * Return: Number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t elem = 0;
+    
+	while (h)
+	{
+		elem++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+
+	return (elem);
+}
