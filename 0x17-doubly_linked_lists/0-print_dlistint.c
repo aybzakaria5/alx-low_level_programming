cad00295dead9b8fa7716628f1f08cbb17205c5e
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-08-18 22:39:49 +0100

    betty fixed

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,10 +1,10 @@
 #include"lists.h"
 
 /**
- * print_dlistint - prints all the elements in a linked list.
- * @h: head of a linked list
- *
- * Return: NUmber of nodes
+* print_dlistint - prints all the elements in a linked list.
+* @h: head of a linked list
+*
+*Return: NUmber of nodes
 */
 size_t print_dlistint(const dlistint_t *h)
 {
