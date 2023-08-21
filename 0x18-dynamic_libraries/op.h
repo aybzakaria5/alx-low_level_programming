886a96a27c560ad3fc7af1809307a0b7d02a8b12
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-08-22 00:02:11 +0100

    craetign a shared library for python with c

---
+++ 0x18-dynamic_libraries/op.h
@@ -0,0 +1,13 @@
+#ifndef OP_H
+#define OP_H
+
+#include <stdio.h>
+
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+
+
+#endif
