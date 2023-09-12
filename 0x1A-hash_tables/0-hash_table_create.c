160a3d7997a38c0419a22517269244ef312b475c
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-09-13 00:42:04 +0100

    fixing that damn betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,11 +10,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *n_table = (hash_table_t *) malloc(sizeof(hash_table_t));
 
-    if (n_table == NULL)
-    {
-        free(n_table);
-        return (NULL);
-    }
+	if (n_table == NULL)
+	{
+		free(n_table);
+		return (NULL);
+	}
 	n_table->size = size;
 	n_table->array = (hash_node_t **) calloc(size, sizeof(hash_node_t *));
 	if (n_table->array == NULL)
