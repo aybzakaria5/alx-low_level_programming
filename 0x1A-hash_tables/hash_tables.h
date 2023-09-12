33b20411ef742b1f7592aa07ee6b3015a9136e61
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-09-13 00:49:50 +0100

    the djb algorithm

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,9 +1,9 @@
-#ifndef HASH_TBALES
+#ifndef HASH_TABLES
 #define HASH_TABLES
-#include <stdlib.h>
-#include  <stdio.h>
-#include <string.h>
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * struct hash_node_s - Node of a hash table
@@ -15,11 +15,10 @@
  */
 typedef struct hash_node_s
 {
-     char *key;
-     char *value;
-     struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
-
 
 /**
  * struct hash_table_s - Hash table data structure
@@ -31,9 +30,11 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-     unsigned long int size;
-     hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
+
 #endif
